aef2089ccaeb510c79f8a7de227bc961bf31df58
power <1410837981@qq.com>
2023-08-11 18:23:01 +0800

    feat: test

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -4,14 +4,14 @@ export default () => {
   const config: PowerPartial<EggAppConfig> = {};
   config.baseUrl = 'prod.url';
   config.security = {
-    domainWhiteList: ['http://power17.xyz'], // cors
+    domainWhiteList: ['https://power17.xyz'], // cors
   };
   // jwt 失效时间
   config.jwtExpires = '2 days';
   config.giteeOauthConfig = {
-    redirectURL: 'http://power17.xyz/api/users/gitee/callback',
+    redirectURL: 'https://power17.xyz/api/users/gitee/callback',
   };
-  config.h5baseUrl = 'http://power17.xyz/api/utils/pages';
+  config.h5baseUrl = 'https://power17.xyz/api/utils/pages';
   // config.mongoose = {
   //   client: {
   //     url: 'mongodb://poster-mongo:27017/poster',
